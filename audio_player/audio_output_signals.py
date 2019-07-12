cba0acb653b691f661d7514113f9bdb605bbecf1
0xf0f <0x0meta@gmail.com>
2019-07-12 17:41:48 +1200

    removed AudioOutput and related classes

--- audio_player/audio_output_signals.py
+++
@@ -1,8 +0,0 @@
-from .lib.signals import SignalList
-
-
-class Signals(SignalList):
-    def __init__(self):
-        super().__init__()
-        self.stream_started = self.add_signal('stream_started')
-        self.stream_stopped = self.add_signal('stream_stopped')
