99fefa16a55abd1cbdec1011d6b1462d5bb4a9d6
0xf0f <0x0meta@gmail.com>
2019-07-12 17:03:01 +1200

    added module containing extensions supported by different adapters

---
+++ audio_player/util/constants/supported_extensions.py
@@ -0,0 +1,25 @@
+
+soundfile_extensions = (
+    '.wav', '.wave', '.flac',
+    '.aiff', '.aif', '.aifc',
+    '.au', '.snd',
+    '.raw',
+    '.paf',
+    '.8svx', '.iff',
+    '.sph', '.nist',
+    '.sf',
+)
+
+openmpt123_extensions = (
+    '.mptm', '.mod', '.s3m', '.xm',
+    '.it', '.669', '.amf', '.ams',
+    '.c67', '.dbm', '.digi', '.dmf',
+    '.dsm', '.dtm', '.far', '.imf',
+    '.ice', '.j2b', '.m15', '.mdl',
+    '.med', '.mms', '.mt2', '.mtm',
+    '.nst', '.okt', '.plm', '.psm',
+    '.pt36', '.ptm', '.sfx', '.sfx2',
+    '.st26', '.stk', '.stm', '.stp',
+    '.ult', '.wow', '.gdm', '.mo3',
+    '.umx', '.xpk', '.ppm', '.mmcmp'
+)
