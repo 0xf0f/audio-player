69e19049229fd727977cdcb3c10aa624e6a92523
0xf0f <0x0meta@gmail.com>
2019-07-12 16:55:38 +1200

    created directory specifically for binaries

---
+++ audio_player/bin/__init__.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+cd = Path(__file__).parent
+
+ffmpeg_directory = cd/'ffmpeg'
+ffprobe_directory = cd/'ffprobe'
+
+openmpt_directory = cd/'openmpt123'
+
+sox_directory = cd/'sox'
+
+timidity_directory = cd/'timidity'
